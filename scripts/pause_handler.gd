4e6710ad983c1da88102b4ee3392b3b3b6be02ec
XTurnDev <xturnyt135@gmail.com>
2025-04-10 21:38:45 +0300

    init

--- scripts/pause_handler.gd
+++ scripts/pause_handler.gd
@@ -3,20 +3,17 @@ extends Node
 @onready var pause_menu: Control = $"../UserInterface/PauseMenu"
 @export var character: CharacterBody3D
 
-var paused : bool = false
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_pause"):
-		if not paused:
+		if not get_tree().paused:
 			if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 				Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 			pause_menu.visible = true
 			get_tree().paused = true
-			paused = true
 		else:
 			if not character.inPc:
 				if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
 					Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 			pause_menu.visible = false
 			get_tree().paused = false
-			paused = false
